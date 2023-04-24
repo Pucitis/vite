c57f80c783f53babe3ee6e0353f1ee8d2984ce81
Martins <pucitis.martins@gmail.com>
2023-04-25 00:49:43 +0300

    gradient change

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -7,7 +7,7 @@ function Intro(){
     return(
        // <FadeIn>
 
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 bg-gradient-to-r from-violet-600 to-indigo-600 min-h-screen">
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 bg-gradient-to-r from-slate-900 to-slate-700 min-h-screen">
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold text-white">
               <div className="flex flex-row"> 
                     <div>
@@ -19,7 +19,7 @@ function Intro(){
                 </div> 
             </h1>
             <p className="text-base md:text-xl mb-3 font-medium text-white">Web izstrādātājs</p>
-            <p>Viss par mani</p>
+            <p className="text-base md:text-xl mb-3 font-medium text-white" >Viss par mani</p>
         </div>
         //</FadeIn>
     )
