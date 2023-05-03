7d09d58183b401f9d39f4046fe62c93ef9e47a3b
Martins <pucitis.martins@gmail.com>
2023-05-03 16:26:29 +0300

    sazinies button and build

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 function Footer(){
     return(
-       <p className="text-sm mt-2  opacity-50">
+       <p className="text-sm mt-2  opacity-70">
 				&copy; {new Date().getFullYear()} Mārtiņš Pūcītis. All rights reserved.
 			</p>
     )
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -10,7 +10,7 @@ function NavBar(){
     
    
     <button className=" text-xl" onClick={() => window.location.reload(false)}>
-      <img src="assets/owl_logo300.png" className="max-h-28" alt=""  style={{filter: `invert(1)`}} />
+      <img src="assets/owl_logo300.png" className=" max-h-16 md:max-h-28" alt=""  style={{filter: `invert(1)`}} />
     </button>
     
   </FadeIn>
@@ -18,15 +18,15 @@ function NavBar(){
   <div className="navbar-center hidden lg:flex">
   <FadeIn>
     <ul className="menu menu-horizontal px-1">
-     <li><a> <Link className='font-medium' to="portfolio" smooth={true} duration={500} >Portfolio</Link> </a></li>
+     <li> <Link className='font-medium' to="portfolio" smooth={true} duration={500} >Portfolio</Link></li>
       
-      <li><a > <Link className='font-medium' to="timeline" smooth={true} duration={500}>Pieredze</Link></a> </li>
+      <li><Link className='font-medium' to="timeline" smooth={true} duration={500}>Pieredze</Link> </li>
     </ul>
 </FadeIn>
   </div>
   <div className="navbar-end">
     <FadeIn>
-    <a className="btn">Get started</a>
+    <a className="btn">Sazinies ar mani</a>
     
   </FadeIn>
   </div>
@@ -34,7 +34,7 @@ function NavBar(){
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-gradient-to-r from-slate-900 to-slate-700 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-gradient-to-r from-slate-900 to-slate-700 rounded-box w-52 lg:hidden">
          <li><a> <Link to="portfolio" smooth={true} duration={500} >Portfolio</Link> </a></li>
         
        <li><a > <Link to="timeline" smooth={true} duration={500}>Timeline</Link></a> </li>
